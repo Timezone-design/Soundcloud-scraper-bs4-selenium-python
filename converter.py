771dba54878b90a687c76f52cd954ab2dbe3e59f
Nikita <nikitanovikov@pullidea.com>
2021-10-20 13:03:13 +0300

    converter log added

--- converter.py
+++ converter.py
@@ -9,6 +9,8 @@ from datetime import datetime
 filenameEmail = path.join(path.dirname(path.abspath(__file__)), 'csv', 'Rappers with Email updated.csv')
 filenameCoupon = path.join(path.dirname(path.abspath(__file__)), 'csv', 'Coupon Codes.csv')
 filenameFinal = path.join(path.dirname(path.abspath(__file__)), 'csv', 'Rappers with Email final.csv')
+filenamePos = path.join(path.dirname(path.abspath(__file__)), 'screenshot_txt', 'position_of_screenshot_for_filenameCoupon.txt')
+filenameLog = path.join(path.dirname(path.abspath(__file__)), 'screenshot_txt', 'screenshot_log.csv')
 
 if not path.exists(filenameCoupon):
   print('Writing a new file for Coupon Code.')
@@ -25,7 +27,7 @@ if not path.exists(filenameFinal):
   finalFile.close()
 
 try:
-  emaildf = pd.read_csv(filenameEmail, encoding='utf-8', header=0, error_bad_lines=False, sep=',')
+  emaildf = pd.read_csv(filenameEmail, encoding='ISO-8859-1', header=0, error_bad_lines=False, sep=',')
 except:
   emaildf = pd.read_csv(filenameEmail, encoding='utf-16', header=0, error_bad_lines=False, sep='\t')
   pass
@@ -63,7 +65,7 @@ newlines['Edd Discount Min Price'] = ''
 newlines['Edd Discount Product Reqs'] = ''
 newlines['Title'] = newlines_all['Email']
 newlines['URL Slug'] = newlines_all['UserName'].replace(' ', '_') + '_' + newlines_all['SongTitle'].apply(lambda x: ''.join(x.lower() for x in str(x).split()[0]))
-newlines['URL Slug'] = newlines['URL Slug'].apply(lambda x: SLUG_FILTER_PATTERN.sub('', x.lower()))
+newlines['URL Slug'] = newlines['URL Slug'].apply(lambda x: SLUG_FILTER_PATTERN.sub('', str(x).lower()))
 newlines['Date'] = datetime.now().strftime("%y-%m-%d %H:%M:%S")
 newlines['Modified Date'] = datetime.now().strftime("%y-%m-%d %H:%M:%S")
 newlines['Status'] = 'active'
@@ -84,7 +86,18 @@ if len(newlines.index) > 0:
   newlines_all['ScreenshotURL'] = ''
   newlines_all.drop('index', axis=1, inplace=True)
 
+  newlines_all.drop(['UserName', 'FullName', 'ArtistName', 'ArtistNameCleaned', 'Location', 'Country', 'Email', 'InstagramUserName', 'InstagramURL', 'HasInstagram', 'SongTitle', 'SongTitleFull', 'GO+', 'SongLink', 'Genre', 'ArtistOrManager', 'NumberOfFollowers', 'Popularity', 'CouponCodeName', 'CouponCode', 'SongPlays', 'UploadDate', 'PopularityAdjusted', 'ActiveState', 'ScreenshotFileName', 'ScreenshotURL'], axis=1).to_csv(filenameLog, mode='a', header=False, encoding='utf-8', sep=',', index=False)
+  print('File log made for screenshot waiting list')
+  
   for index, url in enumerate(newlines_all['SongLink']):
+    with open(filenamePos, 'a', newline='', encoding='utf-8') as f:
+      f.write(newlines_all.iloc[index]['SoundCloudURL'])
+      f.write('\n')
+      f.write(datetime.now().strftime("%y-%m-%d %H:%M:%S"))
+      f.write('\n')
+      f.write("--------------------------------------------------------------")
+      f.write('\n')
+      print('Position logged to txt file: ', newlines_all.iloc[index]['SoundCloudURL'])
     filename = take_screenshot(url, str(newlines_all.iloc[index]['SoundCloudURL']).rsplit('/')[-1], str(newlines_all.iloc[index]['SongTitle']).rsplit()[0], newlines_all.iloc[index]['GO+'])
     if filename != 'None':
       newlines_all.iloc[index, newlines_all.columns.get_loc('ScreenshotFileName')] = filename
